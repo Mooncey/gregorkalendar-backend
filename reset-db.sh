2beceea9622c39400b900d375e917eae2aaf8e8d
matthewkang <matt0410@students.cs.ubc.ca>
2025-01-19 02:33:38 -0800

    update script

--- reset-db.sh
+++ reset-db.sh
@@ -4,22 +4,22 @@
 curl -X POST \
 -H "Content-Type: application/json" \
 -d '{"name": "John Doe","email": "john@example.com"}' \
-http://localhost:5000/api/users
+http://127.0.0.1:5000/api/users
 
 # Add a new user: someone_1
 curl -X POST  \
 -H "Content-Type: application/json" \
 -d '{"name": "someone_1","email": "someone_1@example.com"}' \
-http://localhost:5000/api/users
+http://127.0.0.1:5000/api/users
 
 # Add a new team
 curl -X POST \
 -H "Content-Type: application/json" \
 -d '{"teamName": "CPSC 110","userEmail": "john@example.com"}' \
-http://localhost:5000/api/team
+http://127.0.0.1:5000/api/team
 
 # Add a new member to a team
 curl -X POST \
 -H "Content-Type: application/json" \
 -d '{"teamId": 1,"member": {"email": "someone_1@example.com","name": "someone_1"}}' \
-http://localhost:5000/api/team/member
+http://127.0.0.1:5000/api/team/member
