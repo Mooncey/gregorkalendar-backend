290f03a55679ea25f191502baf74f11b3f781c79
William <william@wyfx.ca>
2025-01-18 22:22:47 -0800

    Update member team infos

--- models/MemberTeamInfos.py
+++ models/MemberTeamInfos.py
@@ -7,13 +7,13 @@ class MemberTeamInfos(db.Model):
     __tablename__ = 'member_team_infos'
     team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), primary_key=True)
     user_email = db.Column(db.String(120), db.ForeignKey('users.email'), primary_key=True)
-    availbility = db.Column(db.JSON, nullable=False)
+    available_blocks = db.Column(db.JSON, nullable=False)
+    prefer_not_blocks = db.Column(db.JSON, nullable=False)
     max_blocks = db.Column(db.Integer, nullable=False)
-    
+ 
     def to_dict(self):
         return {
             'team_id': self.team_id,
             'user_email': self.user_email,
-            'availbility': self.availbility,
             'max_blocks': self.max_blocks
         }
