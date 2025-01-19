41e463e144adf8d15e4ff8a6064dbff1a8d32934
William <william@wyfx.ca>
2025-01-19 02:26:53 -0800

    Fix script

--- reset-db.sh
+++ reset-db.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
 # Add a new user: John Doe
-curl -X POST http://localhost:5000/api/users \
+curl -X POST \
 -H "Content-Type: application/json" \
--d '{
-  "name": "John Doe",
-  "email": "john@example.com"
-}'
+-d '{"name": "John Doe","email": "john@example.com"}' \
+http://localhost:5000/api/users
 
 # Add a new user: someone_1
-curl -X POST http://localhost:5000/api/users \
+curl -X POST  \
 -H "Content-Type: application/json" \
--d '{"name": "someone_1","email": "someone_1@example.com"}'
+-d '{"name": "someone_1","email": "someone_1@example.com"}' \
+http://localhost:5000/api/users
 
 # Add a new team
-curl -X POST http://localhost:5000/api/team \
+curl -X POST \
 -H "Content-Type: application/json" \
--d '{"teamName": "CPSC 110","userEmail": "john@example.com"}'
+-d '{"teamName": "CPSC 110","userEmail": "john@example.com"}' \
+http://localhost:5000/api/team
 
 # Add a new member to a team
-curl -X POST http://localhost:5000/api/team/member \
+curl -X POST \
 -H "Content-Type: application/json" \
--d '{"teamId": 1,"member": {"email": "someone_1@example.com","name": "someone_1"}}'
+-d '{"teamId": 1,"member": {"email": "someone_1@example.com","name": "someone_1"}}' \
+http://localhost:5000/api/team/member
