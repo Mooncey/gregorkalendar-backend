c21e881886e77e8dfca9d83ff2d57c1da13dbbe8
William <william@wyfx.ca>
2025-01-19 05:52:00 -0800

    Add human-readable names to the reset db command

--- reset-db.sh
+++ reset-db.sh
@@ -3,63 +3,63 @@
 # Add a new user: John Doe
 curl -X POST \
 -H "Content-Type: application/json" \
--d '{"name": "John Doe","email": "john@example.com"}' \
+-d '{"name": "Emily Fuchs","email": "emilyef@ubc.ca"}' \
 http://127.0.0.1:5000/api/users
 
 # Add a new user: someone_1
 curl -X POST  \
 -H "Content-Type: application/json" \
--d '{"name": "someone_1","email": "someone_1@example.com"}' \
+-d '{"name": "Matthew Kang","email": "matthew@ubc.ca"}' \
 http://127.0.0.1:5000/api/users
 
 # Add a new user: someone_2
 curl -X POST  \
 -H "Content-Type: application/json" \
--d '{"name": "someone_1","email": "someone_2@example.com"}' \
+-d '{"name": "Susan Chung","email": "susan@ubc.ca"}' \
 http://127.0.0.1:5000/api/users
 
 # Add a new user: someone_3
 curl -X POST  \
 -H "Content-Type: application/json" \
--d '{"name": "someone_1","email": "someone_3@example.com"}' \
+-d '{"name": "Kevin Zhou","email": "kevin@ubc.ca"}' \
 http://127.0.0.1:5000/api/users
 
 # Add a new team
 curl -X POST \
 -H "Content-Type: application/json" \
--d '{"teamName": "CPSC 110","userEmail": "john@example.com"}' \
+-d '{"teamName": "CPSC 110","userEmail": "emilyef@ubc.ca"}' \
 http://127.0.0.1:5000/api/team
 
 # Add a new member to a team
 curl -X POST \
 -H "Content-Type: application/json" \
--d '{"teamId": 1,"member": {"email": "someone_1@example.com","name": "someone_1"}}' \
+-d '{"teamId": 1,"member": {"email": "matthew@ubc.ca","name": "Matthew Kang"}}' \
 http://127.0.0.1:5000/api/team/member
 
 curl -X POST \
 -H "Content-Type: application/json" \
--d '{"teamId": 1,"member": {"email": "someone_2@example.com","name": "someone_2"}}' \
+-d '{"teamId": 1,"member": {"email": "susan@ubc.ca","name": "Susan Chung"}}' \
 http://127.0.0.1:5000/api/team/member
 
 curl -X POST \
 -H "Content-Type: application/json" \
--d '{"teamId": 1,"member": {"email": "someone_3@example.com","name": "someone_3"}}' \
+-d '{"teamId": 1,"member": {"email": "kevin@ubc.ca","name": "Kevin Zhou"}}' \
 http://127.0.0.1:5000/api/team/member
 
 # Add a new availability for someone_1
 curl -X POST \
 -H "Content-Type: application/json" \
--d '{"teamId": 1, "userEmail": "someone_1@example.com", "availableBlocks":[60,61,62,63], "preferNotBlocks": [100,101,102,103]}' \
+-d '{"teamId": 1, "userEmail": "matthew@ubc.ca", "availableBlocks":[60,61,62,63], "preferNotBlocks": [100,101,102,103]}' \
 http://127.0.0.1:5000/api/team/member/availability
 
 curl -X POST \
 -H "Content-Type: application/json" \
--d '{"teamId": 1, "userEmail": "someone_2@example.com", "availableBlocks":[60], "preferNotBlocks": [61, 62, 63, 100,101,102,103]}' \
+-d '{"teamId": 1, "userEmail": "susan@ubc.ca", "availableBlocks":[60], "preferNotBlocks": [61, 62, 63, 100,101,102,103]}' \
 http://127.0.0.1:5000/api/team/member/availability
 
 curl -X POST \
 -H "Content-Type: application/json" \
--d '{"teamId": 1, "userEmail": "someone_3@example.com", "availableBlocks":[100,101,102,103], "preferNotBlocks": []}' \
+-d '{"teamId": 1, "userEmail": "kevin@ubc.ca", "availableBlocks":[100,101,102,103], "preferNotBlocks": []}' \
 http://127.0.0.1:5000/api/team/member/availability
 
 # Add a new slot
